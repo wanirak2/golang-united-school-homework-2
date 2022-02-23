07d4b7811f0ec23636564a0812d6ae3bb964a0d7
danik <denrg94@gmail.com>
2022-02-24 05:21:32 +0600

    songi

--- solution.go
+++ solution.go
@@ -1,5 +1,7 @@
 package square
 
+import "fmt"
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
